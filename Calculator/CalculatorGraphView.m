93780dd8db850323428134722209206617503424
Hon-ming Chen <hmchen@gmail.com>
2012-01-20 13:01:05 -0800

    imported AxesDrawer and used its only method to draw the axes

--- Calculator/CalculatorGraphView.m
+++ Calculator/CalculatorGraphView.m
@@ -7,6 +7,7 @@
 //
 
 #import "CalculatorGraphView.h"
+#import "AxesDrawer.h"
 
 @implementation CalculatorGraphView
 
@@ -19,13 +20,24 @@
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
-    // Drawing code
+    CGContextRef context = UIGraphicsGetCurrentContext();   //always get the context
+    
+    //get mid point of the view
+    CGPoint midPoint;
+    midPoint.x = self.bounds.origin.x + self.bounds.size.width/2;
+    midPoint.y = self.bounds.origin.x + self.bounds.size.height/2;
+    CGFloat pointsPerUnit = 5;
+    [[UIColor blueColor] setStroke];    //set line color
+    //draw axes
+    [AxesDrawer drawAxesInRect:rect originAtPoint:midPoint scale:pointsPerUnit];
 }
-*/
 
 @end
+    
+    
+    
+    
+    
+    
