e590d4a925f3609c1459443d0bef1601c5b02531
Hon-ming Chen <hmchen@gmail.com>
2012-01-19 18:49:06 -0800

    Added CalculatorGraphView and CalculatorGraphViewController files

---
+++ Calculator/CalculatorGraphView.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorGraphView.h
+//  Calculator
+//
+//  Created by Hon-ming Chen on 1/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorGraphView : UIView
+
+@end
---
+++ Calculator/CalculatorGraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorGraphViewController.h
+//  Calculator
+//
+//  Created by Hon-ming Chen on 1/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorGraphViewController : UIViewController
+
+@end
